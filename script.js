b272e5d42e678d53ee928572694b55314322933b
accio-garry <168645773+accio-garry@users.noreply.github.com>
2024-05-11 03:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+
+function daysOfAYear(year){
+	if((year%4)==0){
+		console.log("366");
+		
+	}else{
+		console.log("365");
+		
+	}
+}
+
+daysOfAYear(2022);
+daysOfAYear(2024);
+daysOfAYear(1900);
+daysOfAYear(2000);
